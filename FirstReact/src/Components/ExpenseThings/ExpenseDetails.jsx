9d27237f5507e11fd8943198eef2643234e33163
ranjandhal73 <ranjandhal.doc@gmail.com>
2024-03-10 12:20:40 +0530

    Adding Edit Button

--- FirstReact/src/Components/ExpenseThings/ExpenseDetails.jsx
+++ FirstReact/src/Components/ExpenseThings/ExpenseDetails.jsx
@@ -1,14 +1,23 @@
 import Button from "./Button";
+import { useState } from "react";
 
 const ExpenseDetails = (props) => {
+  const [changeExpense, setchangeExpense] = useState(props.amount);
+  function editHandler(){
+    setchangeExpense(100)
+  }
 
   return (
     <div className="rounded-3xl p-4 items-center w-3/4 flex justify-around text-2xl flex-wrap">
-      <div className=" w-1/3">{props.title}</div>
-        <div className=" w-1/3">{props.location}</div>
-        <div style={{backgroundColor: "#40005d"}}className=" w-2/10 bg-blue-700 rounded-xl text-center py-2 px-4">
-          ${props.amount}
+      <div className=" w-1/5">{props.title}</div>
+        <div className=" w-1/5">{props.location}</div>
+        <div style={{backgroundColor: "#40005d"}}className=" w-1/10 bg-blue-700 rounded-xl text-center py-2 px-4">
+          ${changeExpense}
         </div>
+        {/* <EditBtn name='Edit'/> */}
+        <button className="bg-blue-700 rounded-xl text-center py-2 px-4 border border-white hover:border-opacity-50 "
+        onClick={editHandler}
+        >Edit</button>
         <Button name='Delete'/>
     </div>
   );
