fbee75acc9477deed30b9a98ad50457bcd8a4707
ranjandhal73 <ranjandhal.doc@gmail.com>
2024-03-11 22:19:52 +0530

    Making the expense form input visible on screen

--- FirstReact/src/Components/ExpenseThings/ExpenseForm.jsx
+++ FirstReact/src/Components/ExpenseThings/ExpenseForm.jsx
@@ -1,64 +1,98 @@
 import { useState } from "react";
 
+function ExpenseForm(props) {
+  const [enteredTitle, setenteredTitle] = useState("");
+  const [enteredAmount, setenteredAmount] = useState("");
+  const [enteredDate, setenteredDate] = useState("");
+  // const [enteredLocation, setenteredLocation] = useState("");
 
-function ExpenseForm() {
-    const [enteredTitle, setenteredTitle] = useState('');
-    const [enteredAmount, setenteredAmount] = useState('');
-    const [enteredDate, setenteredDate] = useState('');
+  function titleHandler(e) {
+    setenteredTitle(e.target.value);
+  }
 
-    function formHandler(e){
-      e.preventDefault();
+  function amountHandler(e) {
+    setenteredAmount(e.target.value);
+  }
 
-      const expenseData = {
-        title: enteredTitle,
-        amount: enteredAmount,
-        date: new Date(enteredDate).toLocaleDateString()
-      }
-      console.log(expenseData);
-      setenteredTitle('');
-      setenteredAmount('');
-      setenteredDate('');
+  function dateHandler(e) {
+    setenteredDate(e.target.value);
+  }
+
+  // function locationHandler(e) {
+  //   setenteredLocation(e.target.value);
+  // }
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    const expenseData = {
+      id: Math.random(),
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate).toLocaleDateString(),
+    };
+    if (
+      enteredTitle === "" ||
+      enteredAmount === "" ||
+      enteredDate === ""
+    ) {
+      alert("Entered the Details");
+    } else {
+      // console.log(expenseData);
+      props.addNewData(expenseData);
     }
 
-    function titleHandler(e){
-      setenteredTitle(e.target.value)
-    }
-
-    function amountHandler(e){
-      setenteredAmount(e.target.value)
-    }
-
-    function dateHandler(e){
-      setenteredDate(e.target.value)
-    }
+    setenteredTitle("");
+    setenteredAmount("");
+    setenteredDate("");
+  }
 
   return (
     <>
       <div className="flex items-center justify-center text-xl flex-wrap">
-        <form className="flex gap-2 items-center text-center flex-wrap" 
-        >
-        <label htmlFor="expense-title">Expense title</label>
-        <input type="text" id="expense-title" 
-        className="text-black pl-3 text-xl"
-        value={enteredTitle}
-        onChange={titleHandler}
-        />
+        <form className="flex gap-2 items-center text-center flex-wrap">
+          <label htmlFor="expense-title">Expense title</label>
+          <input
+            type="text"
+            id="expense-title"
+            className="text-black p-0.5 pl-3 text-xl"
+            value={enteredTitle}
+            onChange={titleHandler}
+          />
 
-        <label htmlFor="expense-amount">Expense Amount</label>
-        <input type="text" id="expense-amount" 
-        className="text-black pl-3 text-xl"
-        value={enteredAmount}
-        onChange={amountHandler}  />
+          <label htmlFor="expense-amount">Expense Amount</label>
+          <input
+            type="text"
+            id="expense-amount"
+            className="text-black p-0.5 pl-3 text-xl"
+            value={enteredAmount}
+            onChange={amountHandler}
+          />
 
-        <label htmlFor="expense-Date" >Date</label>
-        <input type="date" id="expense-Date" 
-        className="text-black pl-3 text-xl"
-        value={enteredDate}
-        onChange={dateHandler} />
+          {/* <label htmlFor="expense-amount">Expense Location</label>
+          <input
+            type="text"
+            id="expense-location"
+            className="text-black p-0.5 pl-3 text-xl"
+            value={enteredAmount}
+            onChange={locationHandler}
+          /> */}
 
-        <button className="bg-green-700 rounded-xl text-center py-2 px-4 border border-white hover:border-opacity-50"
-        onClick={formHandler}
-        >Submit</button>
+          <label htmlFor="expense-Date">Date</label>
+          <input
+            type="date"
+            id="expense-Date"
+            className="text-black p-0.5 pl-3 text-xl"
+            value={enteredDate}
+            onChange={dateHandler}
+          />
+
+          <button
+            className="bg-green-700 rounded-xl text-center py-2 px-4 border border-white hover:border-opacity-50"
+            onClick={submitHandler}
+          >
+            Submit
+          </button>
         </form>
       </div>
     </>
