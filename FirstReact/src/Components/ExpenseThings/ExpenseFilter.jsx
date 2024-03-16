84966eb47930fa1377fdf86f649641717d55ce34
ranjandhal73 <ranjandhal.doc@gmail.com>
2024-03-16 20:33:34 +0530

    Add message when only one expense is there

--- FirstReact/src/Components/ExpenseThings/ExpenseFilter.jsx
+++ FirstReact/src/Components/ExpenseThings/ExpenseFilter.jsx
@@ -18,6 +18,7 @@ const dropdownHandler= (e) =>{
           <option value="2021">2021</option>
           <option value="2020">2020</option>
           <option value="2019">2019</option>
+          <option value="2018">2018</option>
         </select>
       </div>
   );
--- FirstReact/src/Components/ExpenseThings/ExpenseItem.jsx
+++ FirstReact/src/Components/ExpenseThings/ExpenseItem.jsx
@@ -1,34 +1,72 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import ExpenseFilter from './ExpenseFilter';
-
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseFilter from "./ExpenseFilter";
 
 const ExpenseItem = (props) => {
-const changeFilterHandler = (year) =>{
+  const changeFilterHandler = (year) => {
     props.onYearChange(year);
-}
-let filteredItem = props.items;
-if (props.year !== null) {
-  filteredItem = props.items.filter((item) => {
-    // return item.date.split("/")[2] === props.year;
-   return item.date.substring(6) === props.year
-  });
-}
+  };
+  let filteredItem = props.items;
+  if (props.year !== null) {
+    filteredItem = props.items.filter((item) => {
+      // return item.date.split("/")[2] === props.year;
+      return item.date.substring(6) === props.year;
+    });
+  }
 
-    return (
-        <>
-            <ExpenseFilter onChangeFilter={changeFilterHandler}/>
-            {filteredItem.map((item)=>{     
-                   return <div key={item.id} style={{ backgroundColor: "#4b4b4b" }} className={`w-3/4 flex items-center justify-between mx-auto my-4 rounded-3xl flex-wrap`}>  
-                        <ExpenseDate date={item.date} />
-                        <ExpenseDetails amount={item.amount} location={item.location} title={item.title} />
-                    </div>   
-                
-                })
+  return (
+    <>
+      <ExpenseFilter onChangeFilter={changeFilterHandler} />
+      {filteredItem.length === 0 && 
+        <div className="text-center mt-36">
+          <div className="text-4xl p-3 rounded-xl bg-gray-800 inline-block">
+            No data available for the year <span className="text-red-500">{props.year}</span>. Please select another Year.
+          </div>
+        </div>
+      }
+
+      {filteredItem.length === 1 && 
+              filteredItem.map((item) => {
+                return (
+                  <div
+                    key={item.id}
+                    style={{ backgroundColor: "#4b4b4b" }}
+                    className={`w-3/4 flex items-center justify-between mx-auto my-4 rounded-3xl flex-wrap`}
+                  >
+                    <ExpenseDate date={item.date} />
+                    <ExpenseDetails
+                      amount={item.amount}
+                      location={item.location}
+                      title={item.title}
+                    />
+                    <p className="text-2xl ml-auto mr-auto">Only single Expense here. Please add more...</p>
+                  </div>
+                  
+                );
+              })
             }
-        </>    
-    );
-}
+
+      {filteredItem.length > 1 && 
+          filteredItem.map((item) => {
+            return (
+              <div
+                key={item.id}
+                style={{ backgroundColor: "#4b4b4b" }}
+                className={`w-3/4 flex items-center justify-between mx-auto my-4 rounded-3xl flex-wrap`}
+              >
+                <ExpenseDate date={item.date} />
+                <ExpenseDetails
+                  amount={item.amount}
+                  location={item.location}
+                  title={item.title}
+                />
+              </div>
+            );
+          })
+      }
+    </>
+  );
+};
 
 export default ExpenseItem;
